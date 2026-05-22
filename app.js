0614ff28343b25c5e7fa83b0b0be2d0858d1cffa
Sahil Dhankhar <https://github.com/Sahildhankhar-arch>
2026-05-22 09:45:01 +0530

    connected frontend to backend

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ window.showPage = function(id) {
 
 async function loadHotels() {
   try {
-    const response = await fetch("http://localhost:5000/api/hotels");
+    const response = await fetch("https://tripmind-production-3598.up.railway.app/api/hotels");
     hotels = await response.json();
 
     if (typeof renderHotelGrid === "function") {
